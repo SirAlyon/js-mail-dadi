6ec48a0094e7812745ca838fab449de3dbefd915
SirAlyon <alessiosada04@gmail.com>
2022-03-29 15:23:02 +0200

    add email input

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,8 +2,22 @@
 Prima di partire a scrivere codice poniamoci qualche domanda: Che ci sia un array da qualche parte? Se dobbiamo confrontare qualcosa che "cosa" ci serve? */
 
 
-const emailList = ['alessio@Boolean.it', 'boolean@boolean.it', 'arrays-es@boolean.it'] 
+const emailList = ['alessio@boolean.it', 'boolean@boolean.it', 'arrays-es@boolean.it'] 
 //Forse non ho capito la consegna dell'esercizio sulla parte della mail (?)
+
+//chiedo all'utente di inserira una mail e ne salvo i dati al click del button
+
+const playNow = document.getElementById('playNow')
+playNow.addEventListener('click', function(){
+    const userEmail = document.getElementById('userEmail').value
+    for (let i=0; i < emailList.length; i++){
+        if (userEmail == emailList[i]){
+            console.log('Ok! Iniziamo a giocare :)')
+        } else {
+            console.log('Per favore inserisci una mail valida per iniziare a giocare!')
+        }
+    }
+})
 
 
 
